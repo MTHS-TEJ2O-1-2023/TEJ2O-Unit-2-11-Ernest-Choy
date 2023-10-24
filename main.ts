0d66335a6f7e6487962d7ea0d65db4e65265666f
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-24 13:05:47 -0400

    typever

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ const numberOne = randint(1, 99)
 const numberTwo = randint(1, 99)
 
 // on button A press
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
   basic.showString(numberOne.toString())
 })
 
@@ -18,10 +18,13 @@ input.onButtonPressed(Button.B, function () {
   basic.showString(numberTwo.toString())
 })
 
-input.onGesture(Gesture.Shake, function() {
-  if(numberOne < numberTwo)
-    basic.showString(numberOne.toString() + "<" + (numberTwo.toString()))
+input.onGesture(Gesture.Shake, function () {
+  if (numberOne < numberTwo) {
+    basic.showString((numberOne).toString() + "<" + (numberTwo).toString())
+  }
+  else {
+    basic.showString((numberOne).toString() + ">" + (numberTwo).toString())
+  }
 
-  else
-    basic.showString(numberOne.toString() + ">" + (numberTwo.toString()))
+basic.showIcon(IconNames.Sad)
 })
