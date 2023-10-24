fb85b977c76d11e141217f5b9563cb0891d91784
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-24 13:18:04 -0400

    typever

--- main.ts
+++ main.ts
@@ -5,23 +5,29 @@
  * This program generates two numbers
 */
 
-const numberOne = randint(1, 99)
-const numberTwo = randint(1, 99)
+// variables
+const numberOne: number = randint(1, 99) 
+const numberTwo: number = randint(1, 99)
 
+// setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 // on button A press
 input.onButtonPressed(Button.A, function () {
-  basic.showString("1" + (numberOne).toString())
+  basic.showString("1:" + (numberOne).toString())
+  basic.showIcon(IconNames.Happy)
 })
 
 // on button B press
 input.onButtonPressed(Button.B, function () {
-  basic.showString("2" + (numberTwo).toString())
+  basic.showString("2:" + (numberTwo).toString())
+  basic.showIcon(IconNames.Happy)
 })
 
+// on Shake
 input.onGesture(Gesture.Shake, function () {
+  basic.clearScreen()
   if (numberOne < numberTwo) {
     basic.showString((numberOne).toString() + "<" + (numberTwo).toString())
   }
