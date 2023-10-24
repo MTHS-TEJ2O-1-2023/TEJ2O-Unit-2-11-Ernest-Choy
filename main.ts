a67ad79088b475d2313e9eb7543ec33d86e63404
Ernest-Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-10-24 13:09:28 -0400

    typever

--- main.ts
+++ main.ts
@@ -8,14 +8,17 @@
 const numberOne = randint(1, 99)
 const numberTwo = randint(1, 99)
 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
 // on button A press
 input.onButtonPressed(Button.A, function () {
-  basic.showString(numberOne.toString())
+  basic.showString("1" + (numberOne).toString())
 })
 
 // on button B press
 input.onButtonPressed(Button.B, function () {
-  basic.showString(numberTwo.toString())
+  basic.showString("2" + (numberTwo).toString())
 })
 
 input.onGesture(Gesture.Shake, function () {
